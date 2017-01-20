c7201091516be17f3c81ab0d998521c983c5d581
Jörg Schwieder <joerg.schwieder@dubusiness.de>
2017-01-20 05:15:27 +0100

    Initial Test Version
    Initial test version: see whether we can print re values

---
+++ squeezerotate.c
@@ -0,0 +1,92 @@
+/*
+
+SqueezeRotate - Sets the volume of a squeezebox player running on a raspberry pi
+
+<C> 2017 Jörg Schwieder
+
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <wiringPi.h>
+
+#include "squeezerotate.h"
+#include "rotaryencoder/rotaryencoder.h"
+
+static volatile int stop_signal;
+static void sigHandler( int sig, siginfo_t *siginfo, void *context );
+
+int main( int argc, char *argv[] ) {
+
+    /*------------------------------------------------------------------------*\
+     OK, from here on we catch some terminating signals and ignore others
+     \*------------------------------------------------------------------------*/
+    struct sigaction act;
+    memset( &act, 0, sizeof(act) );
+    act.sa_sigaction = &sigHandler;
+    act.sa_flags     = SA_SIGINFO;
+    sigaction( SIGINT, &act, NULL );
+    sigaction( SIGTERM, &act, NULL );
+
+    /*------------------------------------------------------------------------*\
+     Setup PID file, ignore errors...
+     \*------------------------------------------------------------------------*/
+    fp = fopen( pid_fname, "w" );
+    if( fp ) {
+        fprintf( fp, "%d\n", getpid() );
+        fclose( fp );
+    }
+    
+
+    /*------------------------------------------------------------------------*\
+     Setup Rotary Encoder
+     \*------------------------------------------------------------------------*/
+    wiringPiSetup() ;
+    struct encoder *encoder = setupencoder(4, 5);
+    
+
+    /*------------------------------------------------------------------------*\
+     Mainloop:
+     \*------------------------------------------------------------------------*/
+    while( !stop_signal ) {
+        printf("encoder value: %d", encoder->value);
+        /*------------------------------------------------------------------------*\
+         Just sleep...
+         \*------------------------------------------------------------------------*/
+        sleep( 1000 );
+        
+    } /* end of: while( !stopflag ) */
+}
+
+/*=========================================================================*\
+ Handle signals
+ \*=========================================================================*/
+static void sigHandler( int sig, siginfo_t *siginfo, void *context )
+{
+    
+    /*------------------------------------------------------------------------*\
+     What sort of signal is to be processed ?
+     \*------------------------------------------------------------------------*/
+    switch( sig ) {
+            
+            /*------------------------------------------------------------------------*\
+             A normal termination request
+             \*------------------------------------------------------------------------*/
+        case SIGINT:
+        case SIGTERM:
+            stop_signal = sig;
+            break;
+            
+            /*------------------------------------------------------------------------*\
+             Ignore broken pipes
+             \*------------------------------------------------------------------------*/
+        case SIGPIPE:
+            break;
+    }
+    
+    /*------------------------------------------------------------------------*\
+     That's it.
+     \*------------------------------------------------------------------------*/  
+} 
---
+++ squeezerotate.h
@@ -0,0 +1,7 @@
+/*
+
+SqueezeRotate - Sets the volume of a squeezebox player running on a raspberry pi
+
+<C> 2017 Jörg Schwieder
+
+*/
