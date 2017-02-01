66fae08c8a610c5f440a1446cdb6983de1ee9eb6
Jörg Schwieder <joerg.schwieder@dubusiness.de>
2017-02-01 05:52:27 +0100

    find MAC

--- squeezerotate.c
+++ squeezerotate.c
@@ -614,6 +614,16 @@ int main( int argc, char *argv[] ) {
     // session-ID?
     //headerList = curl_slist_append(headerList, "x-sdi-squeezenetwork-session: ...")
     
+    
+    
+    // Find MAC
+    uint8_t[6] mac;
+    get_mac(mac);
+    static char macBuf[18];
+    sprintf(macBuf, "%X:%X:%X:%X:%X:%X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
+    MAC = macBuf;
+    
+    
 
     //------------------------------------------------------------------------
     // Setup Rotary Encoder
